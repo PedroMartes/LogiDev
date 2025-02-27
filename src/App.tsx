a45a5eaf082a664b61a1755b25b27023198dd047
Ana Julia Mathias <estrelaanamb0108@gmail.com>
2025-02-26 23:52:36 -0300

    Adicionando imagem da home

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.css'
 import { Cabeca } from "./components/Cabeca"
+import { Slider } from './components/home'
 
 function App() {
   return (
     <>
       <Cabeca />
+      <Slider />
     </>
   )
 
---
+++ src/components/home.tsx
@@ -0,0 +1,8 @@
+// import style from "./cabeca.module.css"
+import img1 from "../assets/img/home1.jpeg"
+
+export function Slider() {
+    return (
+        <img src={img1}/>
+    )
+}
