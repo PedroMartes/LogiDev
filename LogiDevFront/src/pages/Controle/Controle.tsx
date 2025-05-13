26ef34b3916b3111a4cccb174656d2fb57a6bd8a
PedroMartes <pedromartes.apps@gmail.com>
2025-05-13 16:55:49 -0300

    adicionando um botão de apagar produtos

--- LogiDevFront/src/pages/Controle/Controle.tsx
+++ LogiDevFront/src/pages/Controle/Controle.tsx
@@ -24,9 +24,21 @@ export function Controle() {
 
     useEffect(() => {
         axios.get("http://localhost:8080/produtos/get")
-            .then(  response => setData(response.data))
+            .then(response => setData(response.data))
             .catch(error => console.error("Erro ao buscar dados:", error));
     }, []);
+
+    const handleDelete = async (id: number) => {
+        if (window.confirm("Tem certeza que deseja apagar este produto?")) {
+            try {
+                await axios.delete(`http://localhost:8080/produtos/delete/${id}`);
+                setData(data.filter(produto => produto.id !== id));
+            } catch (error) {
+                alert("Erro ao apagar produto!");
+                console.error(error);
+            }
+        }
+    };
 
 
     return (
@@ -50,17 +62,20 @@ export function Controle() {
             </table>
 
             <div>
-
                 {data.length > 0 ? (
                     <ul className={styles.produtoList}>
                         {data.map(produtos => (
-                            <ProductCard nome={produtos.nome}
-                                preco={produtos.preco}
-                                descricao={produtos.descricao}
-                                categoriaNome={produtos.categoria.nome}
-                                fornecedorNome={produtos.fornecedor.nome}
-                                quantidade={produtos.quantidade}
-                            />
+                            <li key={produtos.id}>
+                                <ProductCard
+                                    onDelete={() => handleDelete(produtos.id)}
+                                    nome={produtos.nome}
+                                    preco={produtos.preco}
+                                    descricao={produtos.descricao}
+                                    categoriaNome={produtos.categoria.nome}
+                                    fornecedorNome={produtos.fornecedor.nome}
+                                    quantidade={produtos.quantidade}
+                                />
+                            </li>
                         ))}
                     </ul>
                 ) : (
